09f0e9cc09ee48178f7d4a9ae89902121883b075
luo.reid <luo.reid@gmail.com>
2020-10-27 19:45:33 +0800

    feat: 静态变量

---
+++ Circle.java
@@ -0,0 +1,23 @@
+public class Circle {
+  static double PI = 3.14159265;
+  private int radius;
+
+  public double circumference() {
+    return 2 * PI * radius;
+  }
+
+  public void setRadius(int r) {
+    radius = r;
+  }
+
+  public static void main(String[] args) {
+    Circle c1 = new Circle();
+    c1.setRadius(50);
+    Circle c2 = new Circle();
+    c2.setRadius(10);
+    double circum1 = c1.circumference();
+    double circum2 = c2.circumference();
+    System.out.println("Circle 1 has circumference " + circum1);
+    System.out.print("Circle 2 has circumference " + circum2);
+  }
+}
