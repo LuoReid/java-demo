3f630dfa6071b8f1ea165c6915f05b4e0a07ecf2
luo.reid <luo.reid@gmail.com>
2020-10-27 19:25:20 +0800

    feat: 定义方法

--- Date.java
+++ Date.java
@@ -1,12 +1,60 @@
 public class Date {
   private int day, month, year;
 
-  public void printDate() {
-    System.out.println("The current date is(dd/mm/yy):" + this.day + "/" + this.month + "/" + this.year);
+  Date() {
+    day = 1;
+    month = 1;
+    year = 1998;
+  }
+
+  Date(int i, int j, int k) {
+    day = i;
+    month = j;
+    year = k;
+  }
+
+  Date(Date d) {
+    day = d.day;
+    month = d.month;
+    year = d.year;
+  }
+
+  public String printDate() {
+    // System.out.println("The current date is(dd/mm/yy):" + this.day + "/" +
+    // this.month + "/" + this.year);
+    return (this.day + "/" + this.month + "/" + this.year);
+  }
+
+  public Date tomorrow() {
+    Date d = new Date(this);
+    d.day++;
+    if (d.day > d.daysInMonth()) {
+      d.day = 1;
+      d.month++;
+      if (d.month > 12) {
+        d.month = 1;
+        d.year++;
+      }
+    }
+    return d;
+  }
+  public int daysInMonth(){
+    if(month <=0 || month >12) return -1;
+    switch(month){
+      case 1: case 3: case 5: case 7: case 8: case 10: case 12: return 31;
+      case 4: case 6: case 9: case 11: return 30;
+      default:
+        if((year % 4 == 0 && year % 100 != 0)|| year % 400 == 0) return 29;
+        else return 28;
+    }
   }
 
   public static void main(String[] args) {
-    Date d = new Date();
-    d.printDate();
+    Date d1 = new Date();
+    Date d2 = new Date(28,2,1964);
+    Date d3 = new Date(d2);
+    System.out.println("date d1 is (dd/mm/yy):"+d1.printDate());
+    System.out.println("date d3 is (dd/mm/yy):"+d3.printDate());
+    System.out.println("Its tomorrow is (dd/mm/yy):"+d3.tomorrow().printDate());
   }
 }
---
+++ PointTest.java
@@ -0,0 +1,27 @@
+class Point {
+  public int x, y;
+
+  Point(int x1, int y1) {
+    x = x1;
+    y = y1;
+  }
+
+  Point() {
+    this(0, 0);
+  }
+
+  public void moveTo(int x1, int y1) {
+    x = x1;
+    y = y1;
+  }
+}
+
+public class PointTest {
+  public static void main(String[] args) {
+    Point p = new Point(1, 1);
+    p.x = p.y = 20;
+    System.out.println("p.x = " + p.x + " p.y = " + p.y);
+    p.moveTo(30, 30);
+    System.out.println("p.x = " + p.x + " p.y = " + p.y);
+  }
+}
