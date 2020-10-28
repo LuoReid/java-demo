77861d7efea9ef9bb59173c46fb7703b39d788fa
luo.reid <luo.reid@gmail.com>
2020-10-28 19:07:18 +0800

    feat: 2 dimission array

--- Hello.java
+++ Hello.java
@@ -14,18 +14,19 @@ public class Hello {
     // System.out.println(b2>>>=31);
     // System.out.println(b2>>>=1);
     Hello h = new Hello();
-    h.showErr();
+    // h.showErr();
+    h.printTable();
   }
 
-  void showErr(){
-    int i =0;
-    String greetings [] = {"Hello world!","No,I mean it!","HELLO WORLD!!"};
-    while(i<4){
-      try{
+  void showErr() {
+    int i = 0;
+    String greetings[] = { "Hello world!", "No,I mean it!", "HELLO WORLD!!" };
+    while (i < 4) {
+      try {
         System.out.println(greetings[i]);
-      }catch(Exception e){
+      } catch (Exception e) {
         System.out.println(e.toString());
-      }finally{
+      } finally {
         System.out.println("This is always printed");
       }
       i++;
@@ -41,6 +42,24 @@ public class Hello {
         }
         System.out.print("* ");
       }
+    }
+  }
+
+  void printTable() {
+    int myTable[][] = { { 20, 45, 17, 89 }, { 23, 15, 7, 89 }, { 23, 45, 27, 89 }, { 23, 15, 67, 3 } };
+    int sum, max, maxRow = Integer.MIN_VALUE;
+    max = Integer.MIN_VALUE;
+    for (int row = 0; row < 4; row++) {
+      sum = 0;
+      for (int col = 0; col < 4; col++) {
+        System.out.print(myTable[row][col]);
+        sum += myTable[row][col];
+        if (sum > max) {
+          max = sum;
+          maxRow = row;
+        }
+      }
+      System.out.println("Row " + maxRow + " has the Max sum of " + max);
     }
   }
 }
