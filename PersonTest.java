9abf24ed2e81019401473f07a06eda64e311a946
luo.reid <luo.reid@gmail.com>
2020-10-30 20:23:18 +0800

    feat: object extends

--- PersonTest.java
+++ PersonTest.java
@@ -1,8 +1,7 @@
 import java.util.Vector;
 
-
 abstract class Person {
-  public String name;
+  private String name = "John";
 
   public String getName() {
     return name;
@@ -18,14 +17,18 @@ abstract class Person {
 }
 
 class Employee extends Person {
-  public int employeeNumber;
+  private int employeeNumber;
 
   public int getEmployeeNumber() {
     return employeeNumber;
   }
 
+  public void setEmployeeNumber(int v) {
+    employeeNumber = v;
+  }
+
   void salary() {
-    System.out.println("Salary equals " + basic*3);
+    System.out.println("Salary equals " + basic * 3);
   }
 }
 
@@ -44,12 +47,25 @@ class Manager extends Employee {
   }
 
   void salary() {
-    System.out.println("Salary equals " + basic*5);
+    System.out.println("Salary equals " + basic * 5);
   }
 }
 
 public class PersonTest {
   public static void main(String[] args) {
+    // method3
+    Employee jim = new Manager();
+    jim.setName("Jim");
+    Manager unknown = (Manager) jim;
+    unknown.setEmployeeNumber(543469);
+    unknown.responsibilities = new Vector<String>();
+    unknown.responsibilities.add("Internet project");
+    unknown.responsibilities.add("Internet project1");
+    System.out.println(jim.getName());
+    System.out.println(jim.getEmployeeNumber());
+    System.out.println(unknown.getName());
+    System.out.println(unknown.getEmployeeNumber());
+    System.out.println(unknown.getResponisbilities());
     // Employee jim = new Employee();
     // jim.name = "Jim";
     // jim.employeeNumber = 123456;
@@ -63,16 +79,16 @@ public class PersonTest {
     // System.out.println(betty.getEmployeeNumber());
 
     // method2
-    Employee betty = new Manager();
-    betty.name = "Betty";
-    System.out.println(betty.getName());
-    betty.employeeNumber = 543469;
-    betty.salary();
-    if (betty instanceof Manager) {
-      Manager m = (Manager) betty;
-      m.department = "Test";
-      System.out.println("This is the manager of " + m.department);
-      m.salary();
-    }
+    // Employee betty = new Manager();
+    // betty.name = "Betty";
+    // System.out.println(betty.getName());
+    // betty.employeeNumber = 543469;
+    // betty.salary();
+    // if (betty instanceof Manager) {
+    // Manager m = (Manager) betty;
+    // m.department = "Test";
+    // System.out.println("This is the manager of " + m.department);
+    // m.salary();
+    // }
   }
 }
