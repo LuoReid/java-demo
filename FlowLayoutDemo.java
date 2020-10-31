41e46a62f34e06a83c65a04d5b79b44312538b6a
luo.reid <luo.reid@gmail.com>
2020-10-31 13:44:29 +0800

    feat: FlowLayout

---
+++ FlowLayoutDemo.java
@@ -0,0 +1,26 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+public class FlowLayoutDemo {
+  private JFrame frame;
+  private JButton button1,button2,button3;
+  public static void main(String[] args) {
+    FlowLayoutDemo that = new FlowLayoutDemo();
+    that.go();
+  }
+  public void go(){
+    frame = new JFrame("Flow Layout");
+    Container contentPane = frame.getContentPane();
+    contentPane.setLayout(new FlowLayout());
+    button1 = new JButton("OK");
+    button2 = new JButton("Open");
+    button3 = new JButton("Close");
+    contentPane.add(button1);
+    contentPane.add(button2);
+    contentPane.add(button3);
+    frame.setSize(200,100);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
